088e925ba410b3886061410e97d7c7ab875bdd64
PLAYTAMIZHA97 <140984325+PLAYTAMIZHA97@users.noreply.github.com>
2023-08-06 02:33:30 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -18,7 +18,7 @@ import os, time
 async def rename_start(client, message):
     file = getattr(message, message.media.value)
     filename = file.file_name  
-    if file.file_size > 2000 * 1024 * 1024:
+    if file.file_size > 3000 * 1536 * 1536:
          return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
 
     try:
@@ -152,8 +152,3 @@ async def doc(bot, update):
     await ms.delete() 
     os.remove(file_path) 
     if ph_path: os.remove(ph_path) 
-
-
-
-
-
