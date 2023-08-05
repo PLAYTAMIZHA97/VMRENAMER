2411bc3a0af2dabdf42c7ca14f9ed81ee1249c75
PLAYTAMIZHA97 <140984325+PLAYTAMIZHA97@users.noreply.github.com>
2023-08-06 03:28:35 +0530

    comments.py

---
+++ comments.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("credits", CMD))
+
+async def check_alive(_, message):
+
+    await message.reply_text(" ʜᴀɪ ᴍᴀᴋᴋᴀʟᴇy ɪᴀᴍ.__𝚅𝙲__. ")
+ 
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("ᴛᴀᴍɪʟᴍᴏᴠɪᴇ", CMD))
+
+async def check_alive(_, message):
+
+    await message.reply_text(" @ᴠᴍᴩʟᴀyᴍʀ ")
+ 
+from pyrogram import Client, filters
+
+CMD = ["/", "."]
+
+@Client.on_message(filters.command("ʜᴀɪ", CMD))
+
+async def check_alive(_, message):
+
+    await message.reply_text(" ꜱᴏʟʟᴜɴɢᴀ ɴᴀɴʙᴀ ᴀɴy ᴍᴏᴠɪᴇꜱ ᴄʜᴀɴɴᴇʟ ᴠᴇɴᴜᴍᴀ ᴡᴀɪᴛ.. ꜱᴇɴᴅ ᴩᴀɴᴅʀᴀ ᴍᴀᴋᴋᴀʟᴇy @ᴠᴍᴩʟᴀyᴍʀ ᴊᴏɪɴ ᴋᴇᴇᴩ ꜱᴜᴩᴩᴏʀᴛ ɴᴀɴʙᴀ ")
